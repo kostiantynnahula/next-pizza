4e587c14c57d848dd6787e72258ab8cb882442fc
Kostiantyn Nahula <kostyannagula@gmail.com>
2024-07-28 21:45:38 +0300

    Implemented product modal

---
+++ app/(root)/product/[id]/page.tsx
@@ -0,0 +1,45 @@
+import { Container, GroupVariants, Title } from "@/components/shared";
+import { ProductImage } from "@/components/shared";
+import { prisma } from "@/prisma/prisma-client";
+import { notFound } from "next/navigation";
+
+export default async function ProductPage({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  const product = await prisma.product.findUnique({
+    where: { id: Number(id) },
+    include: {
+      items: true,
+    },
+  });
+
+  if (!product) {
+    return notFound();
+  }
+
+  return (
+    <Container className="flex flex-col my-10">
+      <div className="flex flex-1">
+        <ProductImage imageUrl={product.imageUrl} size={40} />
+
+        <div className="w-[490px] bg-[#f7f6f5] p-7">
+          <Title
+            text={product.name}
+            size="md"
+            className="font-extrabold mb-1"
+          />
+          <p className="text-gray-400">Lorem ipsum dolor sit amet.</p>
+          <GroupVariants
+            items={[
+              { name: "Small", value: "1", disabled: false },
+              { name: "Medium", value: "2", disabled: false },
+              { name: "Large", value: "3", disabled: false },
+            ]}
+          />
+        </div>
+      </div>
+    </Container>
+  );
+}
--- components/shared/index.ts
+++ components/shared/index.ts
@@ -10,3 +10,7 @@ export { CheckboxFiltersGroup } from './checkbox-filters-group';
 export { ProductCard } from './product-card';
 export { ProductsGroupList } from './products-group-list';
 export { SearchInput } from './search-input';
+export { PizzaImage } from './pizza-image';
+export { GroupVariants } from './group-variants';
+export { ChooseProductForm } from './choose-product-form';
+export * from './modals';
