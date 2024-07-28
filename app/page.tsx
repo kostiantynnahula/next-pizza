10276cf021acdf874dbd8b6986a3f66d28db3b05
Kostiantyn Nahula <kostyannagula@gmail.com>
2024-07-28 18:26:44 +0300

    Mapped categories and products

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,30 @@ import {
   Title,
   TopBar,
 } from "@/components/shared";
+import { prisma } from "@/prisma/prisma-client";
 
-export default function Home() {
+export default async function Home() {
+  const categories = await prisma.category.findMany({
+    include: {
+      products: {
+        include: {
+          ingredients: true,
+          items: true,
+        },
+      },
+    },
+  });
+
+  const categoryList = categories.filter(
+    (category) => category.products.length > 0
+  );
+
   return (
     <>
       <Container className="mt-10">
         <Title text="All Pizzas" size="lg" className="font-extrabold" />
       </Container>
-      <TopBar />
+      <TopBar categories={categoryList} />
       <Container className="mt-10 pb-14">
         <div className="flex gap-[80px]">
           <div className="w-[250px]">
@@ -21,74 +37,22 @@ export default function Home() {
 
           <div className="flex-1">
             <div className="flex flex-col gap-16">
-              <ProductsGroupList
-                title="Pizzas"
-                categoryId={1}
-                items={[
-                  {
-                    id: 1,
-                    name: "Margherita",
-                    imageUrl:
-                      "https://media.dodostatic.net/image/r:292x292/11EE7D610D2925109AB2E1C92CC5383C.avif",
-                    items: [{ price: 15 }],
-                  },
-                  {
-                    id: 2,
-                    name: "Margherita",
-                    imageUrl:
-                      "https://media.dodostatic.net/image/r:292x292/11EE7D610D2925109AB2E1C92CC5383C.avif",
-                    items: [{ price: 15 }],
-                  },
-                  {
-                    id: 3,
-                    name: "Margherita",
-                    imageUrl:
-                      "https://media.dodostatic.net/image/r:292x292/11EE7D610D2925109AB2E1C92CC5383C.avif",
-                    items: [{ price: 15 }],
-                  },
-                  {
-                    id: 4,
-                    name: "Margherita",
-                    imageUrl:
-                      "https://media.dodostatic.net/image/r:292x292/11EE7D610D2925109AB2E1C92CC5383C.avif",
-                    items: [{ price: 15 }],
-                  },
-                  {
-                    id: 5,
-                    name: "Margherita",
-                    imageUrl:
-                      "https://media.dodostatic.net/image/r:292x292/11EE7D610D2925109AB2E1C92CC5383C.avif",
-                    items: [{ price: 15 }],
-                  },
-                ]}
-              />
-              <ProductsGroupList
-                title="Combo"
-                categoryId={2}
-                items={[
-                  {
-                    id: 1,
-                    name: "Margherita",
-                    imageUrl:
-                      "https://media.dodostatic.net/image/r:292x292/11EE7D610D2925109AB2E1C92CC5383C.avif",
-                    items: [{ price: 15 }],
-                  },
-                  {
-                    id: 2,
-                    name: "Margherita",
-                    imageUrl:
-                      "https://media.dodostatic.net/image/r:292x292/11EE7D610D2925109AB2E1C92CC5383C.avif",
-                    items: [{ price: 15 }],
-                  },
-                  {
-                    id: 3,
-                    name: "Margherita",
-                    imageUrl:
-                      "https://media.dodostatic.net/image/r:292x292/11EE7D610D2925109AB2E1C92CC5383C.avif",
-                    items: [{ price: 15 }],
-                  },
-                ]}
-              />
+              {categories.map(
+                (category) =>
+                  category.products.length > 0 && (
+                    <ProductsGroupList
+                      key={category.id}
+                      title={category.name}
+                      items={category.products.map((product) => ({
+                        id: product.id,
+                        name: product.name,
+                        imageUrl: product.imageUrl,
+                        items: product.items,
+                      }))}
+                      categoryId={category.id}
+                    />
+                  )
+              )}
             </div>
           </div>
         </div>
--- components/shared/categories.tsx
+++ components/shared/categories.tsx
@@ -2,29 +2,21 @@
 
 import { cn } from "@/lib/utils";
 import { useCategoryStore } from "@/store/category";
+import { Category } from "@prisma/client";
 import React from "react";
 
 type Props = {
+  items: Category[];
   className?: string;
 };
 
-const cats = [
-  { id: 1, name: "Pizzas" },
-  { id: 2, name: "Combo" },
-  { id: 3, name: "Snacks" },
-  { id: 4, name: "Cocktails" },
-  { id: 5, name: "Coffee" },
-  { id: 6, name: "Juices" },
-  { id: 7, name: "Deserts" },
-];
-
-export const Categories: React.FC<Props> = ({ className }) => {
+export const Categories: React.FC<Props> = ({ items, className }) => {
   const categoryActiveId = useCategoryStore((state) => state.activeId);
   return (
     <div
       className={cn("inline-flex gap-1 bg-gray-50 p-1 rounded-2xl", className)}
     >
-      {cats.map(({ name, id }) => (
+      {items.map(({ name, id }) => (
         <a
           key={id}
           href={`#${name}`}
